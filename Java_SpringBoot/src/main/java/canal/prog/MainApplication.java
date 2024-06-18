aa1fadf22b5cc0a6d086ef411f474e4a876c1379
Leinad0202 <104594870+Leinad0202@users.noreply.github.com>
2024-06-18 20:53:11 -0300

    Java_SpringBoot

---
+++ Java_SpringBoot/src/main/java/canal/prog/MainApplication.java
@@ -0,0 +1,15 @@
+package canal.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan("canal.prog")
+@EnableJpaRepositories(basePackages = "canal.prog")
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ Java_SpringBoot/src/main/java/com/mycompany/sistema_contas/Sistema_contas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_contas;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Sistema_contas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
